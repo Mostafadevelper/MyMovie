aeb613070b92fc19c9c7ef12b1de9b96f6d90269
mostafaMillensys <mostafa.Mahmoud@millensys.com>
2025-06-21 15:46:35 +0300

    Initial Commit

---
+++ MyMovie/MyMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieApp.swift
+//  MyMovie
+//
+//  Created by Mostafa Mahmoud on 21/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyMovieTests/MyMovieTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieTests.swift
+//  MyMovieTests
+//
+//  Created by Mostafa Mahmoud on 21/06/2025.
+//
+
+import Testing
+@testable import MyMovie
+
+struct MyMovieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
